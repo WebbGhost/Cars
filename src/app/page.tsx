2a5539cec0b57a2131fe81cace9d56a90119a639
WebbGhost <thewebghost23@gmail.com>
2023-06-09 19:51:15 +0500

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      Welcome to Next js with typescipt eslint prettier and husky setup with
+      custom eslint airbnb code style setup
+    </div>
+  );
+}
+export default Home;
